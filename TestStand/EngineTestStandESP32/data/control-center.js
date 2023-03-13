864dee0dcd3ea70f40b433bd0768344d37f81936
wbrozovic <wbrozovic@gmail.com>
2023-03-12 21:36:34 -0500

    oxidizer gauge

--- TestStand/EngineTestStandESP32/data/control-center.js
+++ TestStand/EngineTestStandESP32/data/control-center.js
@@ -72,3 +72,69 @@ function resetConfimationModal() {
         `;
     countdownModal.style.visibility = "hidden";
 }
+
+function setNumberSpinners() {
+    var numberSpinners = document.querySelectorAll(".spinner-holder");
+
+    numberSpinners.forEach((spinner) => {
+        let id = spinner.attributes['spinner-id'].value;
+
+        customSpinnerHtml = `
+        <div style="position: relative; float: right; position: relative; height: 100%;">
+          <button input-id=${id} class="number-spinner__btn-up number-spinner-btn" onclick="increment(event)">+</button>
+          <button input-id=${id} class="number-spinner__btn-down number-spinner-btn" onclick="decrement(event)">-</button>
+        </div>
+        `;
+
+        spinner.insertAdjacentHTML("beforeend", customSpinnerHtml);
+    });
+}
+
+setNumberSpinners();
+
+function increment(event) {
+    var inputId = event.currentTarget.attributes['input-id'].value;
+    var input = document.getElementById(inputId);
+    var value = parseInt(Number(input.value.replace('%', '')), 10);
+    value = isNaN(value) ? 0 : value;
+    if (value >= 100) return
+    value++;
+    input.value = value + '%';
+
+    updateGuage(value);
+}
+
+function decrement(event) {
+    var inputId = event.currentTarget.attributes['input-id'].value;
+    var input = document.getElementById(inputId);
+    var value = parseInt(Number(input.value.replace('%', '')), 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    if (value <= 0) return
+    value--;
+    input.value = value + '%';
+
+    updateGuage(value);
+}
+
+function appendPercent(event) {
+    var input = event.currentTarget;
+    var value = parseInt(Number(input.value.replace('%', '')), 10);
+    value = isNaN(value) ? 0 : value;
+    input.value = value + '%';
+
+    updateGuage(value);
+}
+
+function updateGuage(value) {
+    var gauge = document.getElementById("gauge-color-overlay");
+    gauge.style.width = value + '%';
+
+    if (value >= 100) {
+      gauge.style.borderTopRightRadius = '4px';
+      gauge.style.borderBottomRightRadius = '4px';
+    } else {
+      gauge.style.borderTopRightRadius = '0';
+      gauge.style.borderBottomRightRadius = '0';
+    }
+}
