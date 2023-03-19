6ecfc4fb7f688a0a5f6b784923c47d2d89136b03
wbrozovic <wbrozovic@gmail.com>
2023-03-19 15:01:43 -0500

    more UI refinements for the test stand

--- TestStand/LCD-ESP32-TestStand/src/main.cpp
+++ TestStand/LCD-ESP32-TestStand/src/main.cpp
@@ -17,8 +17,8 @@
 #define FONT_WIDTH 12
 #define FONT_HEIGHT 18
 
-#define ANGLE_ENCODER_1_IN 37
-#define ANGLE_ENCODER_2_IN 38
+#define OX_ANGLE_ENCODER_1_OUT 37
+#define OX_ANGLE_ENCODER_2_OUT 38
 
 #define THRUST_MEASURE_OUT_1 35
 #define THRUST_MEASURE_OUT_2 32
@@ -28,6 +28,9 @@
 #define OX_VALVE_1_IN 15
 #define OX_VALVE_2_IN 13
 #define OX_VALVE_3_IN 12
+
+#define oxVal 28
+#define oxVal2 25
 
 TFT_eSPI tft = TFT_eSPI();
 //TFT_eSprite spr = TFT_eSprite(&tft); // Sprite class needs to be invoked
@@ -43,8 +46,8 @@ int OxValve1Val3;
 
 void initPins() 
 {
-    pinMode(ANGLE_ENCODER_1_IN, INPUT);
-    pinMode(ANGLE_ENCODER_2_IN, INPUT);
+    pinMode(OX_ANGLE_ENCODER_1_OUT, OUTPUT);
+    pinMode(OX_ANGLE_ENCODER_2_OUT, OUTPUT);
 
     pinMode(THRUST_MEASURE_OUT_1, OUTPUT);
     pinMode(THRUST_MEASURE_OUT_2, OUTPUT);
@@ -61,16 +64,14 @@ void setup()
     Serial.begin(115200);
     
     initPins();
-
-    
-    
 }
 
 void loop()
 {
-    //IgnitionVal = ;
-    Serial.printf("Ignition Value: %d", analogRead(IGNITION_IN));
+    analogWrite(OX_ANGLE_ENCODER_1_OUT, 128);
+    analogWrite(OX_ANGLE_ENCODER_2_OUT, 128);
+    Serial.printf("oxVal_1: %d, oxVal_2: %d\n", oxVal, oxVal2);
     Serial.println();
-    
 
+    delay(50);
 }
